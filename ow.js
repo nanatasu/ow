53ef9214ce5027bf944ed37173ab4cf320471114
nanatasu <servtasu+github@gmail.com>
2016-05-30 23:23:47 +0900

    change design

---
+++ ow.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", ev =>{
+  const trg = document.querySelectorAll(".collapse");
+  for(let i=0; i<trg.length; i++){
+    trg[i].addEventListener("click", function(ev){
+      var tgt = document.getElementById(this.dataset.target);
+      tgt.classList.toggle("is-collapsed");
+      this.firstElementChild.className == "fa fa-caret-down" ? this.firstElementChild.className = "fa fa-caret-up" : this.firstElementChild.className = "fa fa-caret-down";
+    }, false);
+  }
+}, false);
