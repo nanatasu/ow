171b1552930e8cc35a000b6c507c097d5513fb2c
nanatasu <servtasu+github@gmail.com>
2016-05-31 16:41:06 +0900

    test

--- ow.js
+++ ow.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function() {
-  const trg = document.querySelectorAll(".collapse");
+  var trg = document.querySelectorAll(".collapse");
 alert("hoge");
-  for(let i=0; i<trg.length; i++){
+  for(var i=0; i<trg.length; i++){
     trg[i].addEventListener("click", function() {
       var tgt = document.getElementById(this.dataset.target);
       tgt.classList.toggle("is-collapsed");
