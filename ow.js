b1f6735b687cedb336448a2c94fa0969bf7eb3d7
nanatasu <servtasu+github@gmail.com>
2016-05-31 14:35:39 +0900

    test

--- ow.js
+++ ow.js
@@ -1,8 +1,8 @@
-window.addEventListener("load", ev =>{
+window.addEventListener("load", function() {
   const trg = document.querySelectorAll(".collapse");
 alert("hoge");
   for(let i=0; i<trg.length; i++){
-    trg[i].addEventListener("click", function(ev){
+    trg[i].addEventListener("click", function() {
       var tgt = document.getElementById(this.dataset.target);
       tgt.classList.toggle("is-collapsed");
       this.firstElementChild.className == "fa fa-caret-down" ? this.firstElementChild.className = "fa fa-caret-up" : this.firstElementChild.className = "fa fa-caret-down";
